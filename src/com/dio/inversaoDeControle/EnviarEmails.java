37b4ebda23fa03a57344a0477c4a6c96532557b4
Carolina Bartoli <caculeka@hotmail.com>
2022-05-30 18:09:00 -0300

    First Commit

---
+++ src/com/dio/inversaoDeControle/EnviarEmails.java
@@ -0,0 +1,15 @@
+package com.dio.inversaoDeControle;
+
+public class EnviarEmails {
+    public EnviarEmails(String tipo, String endereco, String senha){
+
+    }
+
+    public void retornar(String mensagem){
+        System.out.println("Email enviado.");
+    }
+
+    public static EnviarEmails obterDadosEmail(){
+        return new EnviarEmails("stmp","caculeka@gmail.com", "senha");
+    }
+}
